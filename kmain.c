93c011a6d67b4d5a099ad89a2431e25a7684556c
Ampferl <Jonas@ampferl.com>
2020-03-29 20:49:48 +0200

    Add serial port

--- kmain.c
+++ kmain.c
@@ -1,24 +1,57 @@
 #include "kmain.h"
 #include "io.h"
 
-#define COLOR_GREEN		2
-#define COLOR_GREY		8
-#define COLOR_WHITE		15
-#define COLOR_BLACK		0
+#define COLOR_GREEN			2
+#define COLOR_GREY			8
+#define COLOR_WHITE			15
+#define COLOR_BLACK			0
 
-#define FB_CMD_PORT		0x3D4
-#define FB_DATA_PORT		0x3D5
-#define FB_HIGH_BYTE_CMD	14
-#define FB_LOW_BYTE_CMD		15
+#define FB_CMD_PORT			0x3D4
+#define FB_DATA_PORT			0x3D5
+#define FB_HIGH_BYTE_CMD		14
+#define FB_LOW_BYTE_CMD			15
+
+#define SERIAL_COM1_BASE        	0x3F8
+
+#define SERIAL_DATA_PORT(base)          (base)
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+#define SERIAL_LINE_ENABLE_DLAB         0x80
 
 int main()
 {
 	char name[] = "Jonas: Soon I have an own Operating System :D";
-	write(name);
+	fb_write(name);
 	return 0;
 }
 
-void write(char *buf)
+void serial_write()
+{
+	serial_configure_baud_rate(SERIAL_COM1_BASE, 2);
+	serial_configure_line(SERIAL_COM1_BASE);
+	serial_is_transmit_fifo_empty(SERIAL_COM1_BASE);
+}
+
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
+{
+	outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
+	outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
+	outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
+}
+
+void serial_configure_line(unsigned short com)
+{
+	outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
+}
+
+int serial_is_transmit_fifo_empty(unsigned int com)
+{
+	return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
+}
+
+void fb_write(char *buf)
 {
 	char * buffer = buf;
 	int i = 0;
