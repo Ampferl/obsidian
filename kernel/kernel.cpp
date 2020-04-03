d46777236174c750faf38ce59416c80b52c9c346
Ampferl <Jonas@ampferl.com>
2020-04-03 10:48:18 +0200

    Modified printf statement

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -5,14 +5,45 @@ void printf(char* str)
 {
         static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-        for(int i = 0; str[i] != '\0'; ++i)
-                VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+	static uint8_t x=0;
+	static uint8_t y=0;
 
+        for(int i = 0; str[i] != '\0'; ++i)
+        {
+		switch(str[i])
+		{
+			case '\n':
+				y++;
+				x = 0;
+				break;
+			default:
+				VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+				x++;
+				break;
+		}
+
+		if(x >= 80)
+		{
+			y++;
+			x = 0;
+		}
+
+		if(y >= 25)
+		{
+			for(y = 0; y < 25; y++)
+				for(x = 0; x < 80; x++)
+					VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+			x = 0;
+			y = 0;
+		}
+	}
 }
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-	printf("Jonas ist nice xD");
+	printf("Jonas ist nice xD ");
+	printf("Test\n");
+	printf("Ein Test");
 	
 	GlobalDescriptorTable gdt;
 
