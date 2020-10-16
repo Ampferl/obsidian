f9b4eed01e02ccbfd5f02e8d26a310e6d73573b3
Jonas Ampferl <jonas@ampferl.com>
2020-10-16 13:48:59 +0200

    Added simple gui framework

---
+++ kernel/common/graphicscontext.h
@@ -0,0 +1,14 @@
+#ifndef __GRAPHICSCONTEXT_H
+#define __GRAPHICSCONTEXT_H
+
+#include "../drivers/vga.h"
+
+namespace obsidian
+{
+    namespace common
+    {
+        typedef GraphicsContext VideoGraphicsArray;
+    }
+}
+
+#endif//__GRAPHICSCONTEXT_H
