416e4d89bc4fb6b933bf0313e88cec1f387eb668
Jonas Ampferl <jonas@ampferl.com>
2020-10-16 10:27:26 +0200

    Added Mouse support

--- kernel.cpp
+++ kernel.cpp
@@ -2,6 +2,7 @@
 #include "gdt.h"
 #include "interrupts.h"
 #include "keyboard.h"
+#include "mouse.h"
 
 void printf(char* str){
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
@@ -56,6 +57,7 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
 
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
+    MouseDriver mouse(&interrupts);
     KeyboardDriver keyboard(&interrupts);
     interrupts.Activate();
 
